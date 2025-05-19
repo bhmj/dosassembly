de8bbfe6c82b0e058e2f52e01a3c21400cbfcd2b
Michael Gurov <i.gourov@gmail.com>
2025-05-19 14:34:06 +0200

    support multiple domains

--- scripts/install_nginx_config.sh
+++ scripts/install_nginx_config.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-CONF=$1
-TARGET_NAME=$2
-docker network connect ${TARGET_NAME}_net nginx-proxy
-export_vars=$(grep -oE '\$\{[A-Z0-9_]+\}' $CONF | sort -u | tr '\n' ' ')
-envsubst "$export_vars" < $CONF > /var/nginx-proxy/configs/${TARGET_NAME}.conf
+
+CONF_MASK=$1
+NAMESPACE=$2
+
+docker network connect ${NAMESPACE}_net nginx-proxy
+
+for CONF in $CONF_MASK; do
+  if [[ -f "$CONF" ]]; then
+    export_vars=$(grep -oE '\$\{[A-Z0-9_]+\}' "$CONF" | sort -u | tr '\n' ' ')
+    CONF_BASENAME=$(basename "$CONF")
+    envsubst "$export_vars" < "$CONF" > /var/nginx-proxy/configs/${CONF_BASENAME}
+  else
+    echo "Skipping '$CONF' : not a regular file."
+  fi
+done
+
 docker exec nginx-proxy nginx -s reload

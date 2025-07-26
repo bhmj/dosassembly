bb049210d62eff71b2389599585d572b80c416c7
Michael Gurov <i.gourov@gmail.com>
2025-07-26 21:09:20 +0200

    fix wamma_drunk file link

--- sql/Migrations/2025-05-23 073400 examples 2.sql
+++ sql/Migrations/2025-05-23 073400 examples 2.sql
@@ -2,7 +2,7 @@ INSERT INTO public.examples (txt_filename, image_filename, size_category, asm_ty
 VALUES
 ('cantorus.asm', 'cantorus.png', '512b', 'FASM', '*cantorus* by *Titan*, nov 2007, 3nd at Main 2007', 'https://www.pouet.net/prod.php?which=33169', 73),
 ('infinito.asm', 'infinito.png', '512b', 'FASM', '*Infinito, a little bit of.*, apr 2006', 'https://www.pouet.net/prod.php?which=24447', 76),
-('wamma_dunk.asm', 'wamma_drunk.png', '512b', 'NASM', '*I''m afraid I was very, very drunk* by *wamma*, sep 2013', 'https://www.pouet.net/prod.php?which=61910', 94),
+('wamma_drunk.asm', 'wamma_drunk.png', '512b', 'NASM', '*I''m afraid I was very, very drunk* by *wamma*, sep 2013', 'https://www.pouet.net/prod.php?which=61910', 94),
 ('tube.asm', 'tube.gif', '256b', 'NASM', '*tube* by *baze/3SC*, aug 2001, 1st at Syndeecate 2001', 'https://www.pouet.net/prod.php?which=3397', 88),
 ('lattice.asm', 'lattice.gif', '256b', 'NASM', '*lattice* by *baze/3SC*, dec 2001, 1st at Demobit 2001', 'https://www.pouet.net/prod.php?which=4659', 96),
 ('puls.asm', 'puls.png', '256b', 'NASM', '*puls* by *Řrřola*, sep 2009, 1st at Riverwash 2009', 'https://www.pouet.net/prod.php?which=53816', 99),
